b48f85cc9bb23b527a714ce271eeb960f7fd2134
Masudur Rahman <masudrahaman807@gmail.com>
2023-05-23 10:19:40 +0600

    Second Commit

--- lib/screen/loading_screen.dart
+++ lib/screen/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 
 class LoadingScreen extends StatefulWidget {
@@ -40,6 +41,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     @override
   void initState() {
+      void getData() async {
+        var url = Uri.parse('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1');
+        var response = await http.get(url);
+
+        if (response.statusCode == 200) {
+          // Successful request
+          var data = response.body;
+          // Process the data or update your UI
+        } else {
+          // Request failed
+          print('Request failed with status: ${response.statusCode}.');
+        }
+      }
     super.initState();
     _getCurrentLocation().then((Position position) {
       // Handle the retrieved location
@@ -49,41 +63,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
           // Handle any errors that occur during the location retrieval process
         print(e);
    });
+    print(getData);
   }
 
 
-  @override
+    @override
   Widget build(BuildContext context) {
+
     return Scaffold();
 
   }
 }
 
-
-
-
-// body: SafeArea(
-// child: Center(
-// child: Column(
-// mainAxisAlignment: MainAxisAlignment.center,
-// children: [
-// ElevatedButton(
-// onPressed: () {
-// //display loader
-// _getCurrentLocation().then((Position position) {
-// // Handle the retrieved location
-// print('Latitude: ${position.latitude}');
-// print('Longitude: ${position.longitude}');
-// }).catchError((e) {
-// // Handle any errors that occur during the location retrieval process
-// print(e);
-// });
-//
-// },
-// child: Text('Get Location'),
-// ),
-// ],
-// ),
-// ),
-// ),
-//
